0920ad803ba027a5fa684a98327241ddb68371e6
Laura Bergoens <laura.bergoens@pix.fr>
2023-01-02 16:04:00 +0100

    sum for overflowing integers

--- src/sum.ts
+++ src/sum.ts
@@ -1,10 +1,24 @@
-const regexPositiveInteger = /^[0-9]*$/
+import { reverse, stripLeadingZeros, charParseInt } from './utils'
+
+const REGEX_POSITIVE_INTEGER = /^[0-9]*$/
 
 export const sum = (a: string, b: string): string => {
-  if (a.match(regexPositiveInteger)?.[0] !== a) return ''
-  if (b.match(regexPositiveInteger)?.[0] !== b) return ''
+  if (a.match(REGEX_POSITIVE_INTEGER)?.[0] !== a) return ''
+  if (b.match(REGEX_POSITIVE_INTEGER)?.[0] !== b) return ''
   const intA = parseInt(a)
   const intB = parseInt(b)
   if (Number.isSafeInteger(intA) && Number.isSafeInteger(intB)) return (intA + intB).toString()
-  return 'coucou'
+  const reverseA = reverse(stripLeadingZeros(a))
+  const reverseB = reverse(stripLeadingZeros(b))
+  const largest = reverseA.length > reverseB.length ? reverseA : reverseB
+  const smallest = reverseA.length > reverseB.length ? reverseB : reverseA
+  let carry = 0
+  const result = []
+  for (let i = 0; i < largest.length; ++i) {
+    const currentSum = charParseInt(smallest[i]) + charParseInt(largest[i]) + carry
+    carry = Math.floor(currentSum / 10)
+    result.unshift((currentSum % 10).toString())
+  }
+  result.unshift(carry.toString())
+  return stripLeadingZeros(result.join(''))
 }
