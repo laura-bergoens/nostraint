39b490b2d54e7083a4cb82e1d10b944501ac3375
Laura Bergoens <laura.bergoens@pix.fr>
2023-01-04 12:08:52 +0100

    reorganise code to easily test various algorithms

--- tests/add.spec.ts
+++ tests/add.spec.ts
@@ -1,12 +1,12 @@
 import { assert } from 'chai'
-import { sum } from '../src/sum'
+import { add } from '../src'
 
-describe('sum', () => {
+describe('add', () => {
   context('invalid operands', () => {
     context('operand "a"', () => {
       context('when operand is not a valid string formatted positive integer', () => {
         it('should return an empty string', () => {
-          const result = sum('12not an integer', '123')
+          const result = add('12not an integer', '123')
           assert.equal(result, '')
         })
       })
@@ -14,7 +14,7 @@ describe('sum', () => {
     context('operand "b"', () => {
       context('when operand is not a valid string formatted positive integer', () => {
         it('should return an empty string', () => {
-          const result = sum('123', '12not an integer')
+          const result = add('123', '12not an integer')
           assert.equal(result, '')
         })
       })
@@ -24,14 +24,14 @@ describe('sum', () => {
   context('valid operands', () => {
     context('safe integers', () => {
       it('should return the sum of the two operands', () => {
-        const result = sum('57824', '352874')
+        const result = add('57824', '352874')
         assert.equal(result, '410698')
       })
     })
     context('unsafe integers / over Number.MAX_SAFE_INTEGER', () => {
       it('should return the sum of the two operands', () => {
         // Number.MAX_SAFE_INTEGER -> 9007199254740991
-        const result = sum('0032087199264843995', '96057297254240990')
+        const result = add('0032087199264843995', '96057297254240990')
         assert.equal(result, '128144496519084985')
       })
     })
