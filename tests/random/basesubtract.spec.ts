3ed7f60a4bddf7c026f467e55f81594b3f27c44f
Laura BERGOENS <48727874+laura-bergoens@users.noreply.github.com>
2023-01-06 11:55:28 +0100

    bugfix: some addition or subtraction were wrong due to wrong evaluation of final sign (#14)

---
+++ tests/random/basesubtract.spec.ts
@@ -0,0 +1,18 @@
+import { assert, randomIntAsStringAndNumber } from '../test-helper'
+import { baseSubtract_forRandomTestsOnly, algorithmMapper } from '../../src/addition'
+
+const LOOP_COUNT = 100
+
+describe('random baseSubtract', () => {
+  it(`should return the expect subtraction result for random numbers ${LOOP_COUNT} times`, () => {
+    for (const fnc of Object.values(algorithmMapper)) {
+      for (let i = 0; i < LOOP_COUNT; ++i) {
+        const { numberVer: numA, stringVer: argA } = randomIntAsStringAndNumber()
+        const { numberVer: numB, stringVer: argB } = randomIntAsStringAndNumber()
+        const result = baseSubtract_forRandomTestsOnly(argA, argB, fnc)
+        assert.equal(result, (numA - numB).toString(), `"${argA} - ${argB}" should equal "${numA} - ${numB} = ${numA - numB} (str version :${(numA - numB).toString()}), got ${result}`)
+      }
+    }
+    assert.equal(true, true)
+  })
+})
