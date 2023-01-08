5efcdef33748769f6cdafdea339af7864022d8d3
Laura Bergoens <laura.bergoens@pix.fr>
2023-01-08 07:12:10 +0100

    modify perf for multiply to do with negative numbers

--- scripts/performance/multiply.ts
+++ scripts/performance/multiply.ts
@@ -5,7 +5,9 @@ const EXECUTION_COUNT = 1000000
 const SAFE_ARG_A = '+00456871'
 const SAFE_ARG_B = ' 9851'
 const UNSAFE_POS_ARG_A = '+0084 5398236339685583537 125951891419874 0132068978704156'
+const UNSAFE_NEG_ARG_A = '- 0084 5398236339685583537 125951891419874 0132068978704156'
 const UNSAFE_POS_ARG_B = '01089525498544 141443548925735425804120189789041260489'
+const UNSAFE_NEG_ARG_B = '-01089525498544 141443548925735425804120189789041260489'
 
 const bench = (): any => {
   console.log('START - MULTIPLY')
@@ -25,6 +27,22 @@ const bench = (): any => {
     console.log(`\t\t${algorithm} on ${EXECUTION_COUNT} executions : ${endTime - startTime} milliseconds`)
   }
   console.log('\tUNSAFE POSITIVE INTEGERS - END')
+  console.log('\tUNSAFE NEGATIVE INTEGERS')
+  for (const [algorithm, fnc] of Object.entries(algorithmMapper)) {
+    const startTime = performance.now()
+    for (let i = 0; i < EXECUTION_COUNT; ++i)baseMultiply(UNSAFE_NEG_ARG_A, UNSAFE_NEG_ARG_B, fnc)
+    const endTime = performance.now()
+    console.log(`\t\t${algorithm} on ${EXECUTION_COUNT} executions : ${endTime - startTime} milliseconds`)
+  }
+  console.log('\tUNSAFE NEGATIVE INTEGERS - END')
+  console.log('\tUNSAFE INTEGERS')
+  for (const [algorithm, fnc] of Object.entries(algorithmMapper)) {
+    const startTime = performance.now()
+    for (let i = 0; i < EXECUTION_COUNT; ++i)baseMultiply(UNSAFE_NEG_ARG_A, UNSAFE_POS_ARG_B, fnc)
+    const endTime = performance.now()
+    console.log(`\t\t${algorithm} on ${EXECUTION_COUNT} executions : ${endTime - startTime} milliseconds`)
+  }
+  console.log('\tUNSAFE INTEGERS - END')
   console.log('END - MULTIPLY')
 }
 
